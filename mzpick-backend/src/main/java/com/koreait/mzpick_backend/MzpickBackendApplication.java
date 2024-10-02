61045318a2c8cf68135c589f4ad66b579f212367
devyunie <myha001227@gmail.com>
2024-10-02 20:18:13 +0900

    spring 초기 설정 및 [주소:4000/] Sever on 시각화 페이지 추가

--- mzpick-backend/src/main/java/com/koreait/mzpick_backend/MzpickBackendApplication.java
+++ mzpick-backend/src/main/java/com/koreait/mzpick_backend/MzpickBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MzpickBackendApplication {
+public class MzPickBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MzpickBackendApplication.class, args);
+		SpringApplication.run(MzPickBackendApplication.class, args);
 	}
 
 }
