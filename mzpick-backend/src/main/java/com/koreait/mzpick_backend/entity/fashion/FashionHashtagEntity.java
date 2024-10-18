8ecf9d2e50e9ceb34a83dd288633ba02b901fe26
nobodys5 <samudi2@naver.com>
2024-10-18 11:24:13 +0900

    최종 엔티티 수정

--- mzpick-backend/src/main/java/com/koreait/mzpick_backend/entity/fashion/FashionHashtagEntity.java
+++ mzpick-backend/src/main/java/com/koreait/mzpick_backend/entity/fashion/FashionHashtagEntity.java
@@ -1,6 +1,9 @@
 package com.koreait.mzpick_backend.entity.fashion;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +17,8 @@ import lombok.Setter;
 @Entity(name="fashionHashtag")
 @Table(name="fashion_hashtag")
 public class FashionHashtagEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer fashionHashtagNumber;
     private Integer fashionNumber;
     private String fashionHashtagContent;
